ee4bcefbf1d0c652bfdba79907749f0c7ea10af5
akisame338 <akisame338@gmail.com>
2017-10-08 14:41:35 +0900

    ホームディレクトリにドットファイルのシンボリックリンクを生成するスクリプトを作成

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CWD=`pwd`
+DOT_FILES=`find $CWD -type f | grep -E "$CWD/\." | grep -v -E "$CWD/\.git" | sed s:$CWD/::g`
+for file in $DOT_FILES
+do
+    ln -sfnv $CWD/$file $HOME/$file
+done
